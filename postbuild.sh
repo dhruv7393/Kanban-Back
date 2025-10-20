47638e81078e19c9de519e74dd0fa9c41343bab9
Dhruv Desai <dhruvdesai@dhruvs-air-2.mynetworksettings.com>
2025-10-20 02:33:13 -0400

    Fixing postbuild

--- postbuild.sh
+++ postbuild.sh
@@ -7,8 +7,6 @@ mkdir -p ./.amplify-hosting/compute
 cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
-cp -r public ./.amplify-hosting/static
-
 cp deploy-manifest.json ./.amplify-hosting/
 
 cp -r ./src ./.amplify-hosting/compute/default
