105494e3f9ce7736822159008056ca145351e021
Dhruv Desai <dhruvdesai@dhruvs-air-2.mynetworksettings.com>
2025-10-20 02:32:09 -0400

    Possible fix

---
+++ postbuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf ./.amplify-hosting
+
+mkdir -p ./.amplify-hosting/compute
+
+cp -r ./dist ./.amplify-hosting/compute/default
+cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
+
+cp -r public ./.amplify-hosting/static
+
+cp deploy-manifest.json ./.amplify-hosting/
+
+cp -r ./src ./.amplify-hosting/compute/default
